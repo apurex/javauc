afa7bc78cc3c51db1fafaf1483c061a1ee7793eb
apurex <taijomi17@gmail.com>
2017-03-30 20:48:20 -0400

    version 1.0

---
+++ javaejemplo/src/javaejemplo/Javaejemplo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaejemplo;
+
+import static java.time.Clock.system;
+
+/**
+ *
+ * @author Joel
+ */
+public class Javaejemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int edad;
+        String nom;
+        
+        System.out.println("Probando Git");
+        
+    }
+    
+}
